242b43d0c551ea084db20cfa9c143c8857463f8a
31boso <31boso@gmail.com>
2016-06-06 15:21:21 +0200

    modif getIP

--- getIP.php
+++ getIP.php
@@ -2,6 +2,6 @@
  header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
  header('Access-Control-Allow-Credentials: true');
  header('Access-Control-Max-Age: 86400');
-<br>    $ip´= $_SERVER['REMOTE_ADDR'];
+"<br />"    $ip= $_SERVER['REMOTE_ADDR'];
 echo $ip;
 ?>
