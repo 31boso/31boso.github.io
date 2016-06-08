3cba9353454c0d1da71aae84d14af56cf733d66b
31boso <31boso@gmail.com>
2016-06-08 19:27:25 +0200

    add content

--- getIP.php
+++ getIP.php
@@ -1,7 +1,3 @@
 ﻿<?php
- header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
- header('Access-Control-Allow-Credentials: true');
- header('Access-Control-Max-Age: 86400');
-<br>    $ip= $_SERVER['REMOTE_ADDR'];
-return $ip;
+return "ip";
 ?>
